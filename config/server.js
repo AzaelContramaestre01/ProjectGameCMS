4f45f920e82e51fc80535c80aa7e7f7ddd9fa36e
Azael Contramaestre <galaxyhhd@hotmail.com>
2022-01-09 21:26:34 -0300

    feat/azael-project-cms

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e6693b376d2921362cc9128b189e9b1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb662115-f40e-451c-b48f-0ebe804eea00'
+};
